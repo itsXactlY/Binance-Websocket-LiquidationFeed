a78b57a8a9261249d0e2967f2777966d91d30583
itsaLca <6356217+itsaLca@users.noreply.github.com>
2022-04-01 23:22:55 +0200

    Update LiqFeeder.py
    clean up

--- LiqFeeder.py
+++ LiqFeeder.py
@@ -2,21 +2,18 @@ from unicorn_binance_websocket_api.manager import BinanceWebSocketApiManager
 from datetime import datetime
 import json
 
-def load_symbols(coins):
-    symbols = []
-    for coin in coins:
-        symbols.append(coin['symbol'])
-    return symbols
-    
+
 def check_liquidations():
     print("Launching Binance Websocket Connection & awaiting new Liquidations...")
+    
+    nonce = 0
+    
     binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com-futures")
     binance_websocket_api_manager.create_stream(['!forceOrder'], [{}])
-
-    nonce = 0
+    
+    
     while (True):
         lick_stream = binance_websocket_api_manager.pop_stream_data_from_stream_buffer()
-        #settings update script
         cycles = 5000000
         nonce += 1
         if nonce > cycles:
@@ -33,7 +30,7 @@ def check_liquidations():
                 now = datetime.now()
                 past = now - d1
                 duration = past.total_seconds()
-                if duration < 1:
+                if duration < 2:
                     print("---------------------------------------------------------------------------------")
                     print("Liquidation found for:", amount, "Contracts worth: $", lick_size, "on ", symbol)
 
